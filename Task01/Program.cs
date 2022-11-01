1de796e9a7977c03a4f80b4aaf2d668069005bdd
Farida <faridamarasulova6@gmail.com>
2022-11-01 13:02:57 +0300

    add Program.cs

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,22 +1,21 @@
-﻿/*1. Напишите программу, которая  на вход принимает два 
-числа и проверяет, является ли первое число квадратом 
-второго
+﻿/*1.Напишите программу, которая  на вход принимает два числа и 
+// проверяет, является ли первое число квадратом второго
 
-a = 25, b = 5 -> да
-a = 2, b =18 -> нет
-a = 9, b = -3 -> да
-a =-3, b = 9 -> нет*/
+// a = 25, b = 5 -> да
+// a = 2, b =18 -> нет
+// a = 9, b = -3 -> да
+// a =-3, b = 9 -> нет*/
 
 
-Console.WriteLine("Введите два числа: ");
-int number1 = Convert.ToInt32(Console.Readline());
-int number2 = Convert.ToInt32(Console.Readline());
+System.Console.WriteLine("Введите два числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 == number2 * number2)
 {
-    Console.Writeline("Да");
+    Console.WriteLine("Да");
 }
 else
 {
-    Console.Writeline("Нет");
+    Console.WriteLine("Нет");
 }
 
