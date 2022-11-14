2f38e7c742272353823f24d92623f51171b66292
Farida <faridamarasulova6@gmail.com>
2022-11-14 09:12:00 +0300

    Added Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите цикл, который принимает на 
+// 1. вход два числа (A и B) и
+// 2. возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+// 
+
+Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int sum = a;
+int res = b;
+if (b >= 0)
+{
+    Console.WriteLine("Число не натуральное, введите натуральное число");
+}
+for (int i = 1; i < b; i++)
+{
+sum = sum * a;
+}
+Console.WriteLine("A в степени B = " + sum);
+
